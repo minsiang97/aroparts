869fb9b4a16a21905b523e3e6e8282286b71ae39
Ong Min Siang <ongminsiang@gmail.com>
2020-12-18 15:57:50 +0800

    modified api and table

--- models/product.py
+++ models/product.py
@@ -4,7 +4,7 @@ import peewee as pw
 
 class Product(BaseModel):
     name=pw.CharField(unique=True, null=False)
-    description=pw.CharField(null=False)
+    description=pw.TextField(null=False)
     category=pw.ForeignKeyField(Category, backref="products", on_delete = "CASCADE")
     price=pw.CharField(null=False)
     image_path=pw.CharField(null=True)
