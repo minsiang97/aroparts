27e0641192acbefc15a30f3a03e7de42d2c5cd25
Ong Min Siang <ongminsiang@gmail.com>
2021-01-05 13:56:32 +0800

    modified name field in SubCategory table

--- models/sub_category.py
+++ models/sub_category.py
@@ -3,6 +3,6 @@ from models.category import Category
 import peewee as pw
 
 class SubCategory(BaseModel):
-    name=pw.CharField(unique=True, null=False)
+    name=pw.CharField(null=False)
     category = pw.ForeignKeyField(Category, backref="sub-categories", on_delete="CASCADE", null=True)
     
